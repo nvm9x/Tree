c2b12b22eec6db92395235405bf8b369fde5a62f
Zeinep <lzeinep@gmail.com>
2024-04-08 14:46:48 +0600

    application cycle menu

--- src/main/java/kz/runtime/Application.java
+++ src/main/java/kz/runtime/Application.java
@@ -5,38 +5,48 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Выбрать действие");
-        System.out.println("1-создать категорию");
-        System.out.println("2- проверить наличие категории в списке");
-        System.out.println("3- перемещение категорию");
-        System.out.println("4- удалить категорию");
+        boolean infinite = true;
+
+        while (infinite) {
+            System.out.println("Выбрать действие");
+            System.out.println("1-создать категорию");
+            System.out.println("2- проверить наличие категории в списке");
+            System.out.println("3- перемещение категорию");
+            System.out.println("4- удалить категорию");
+            System.out.println("5 - выйти из меню");
 
 
-        int num = Integer.parseInt(scanner.nextLine());
+            int num = Integer.parseInt(scanner.nextLine());
 
-        switch (num) {
-            case 1:
-                System.out.println("Создаем категорию");
-                Create.createCategory();
-                break;
-            case 2:
-                System.out.println("проверить наличие категории в списке");
-                Find.find();
-                break;
+            switch (num) {
+                case 1:
+                    System.out.println("Создаем категорию");
+                    Create.createCategory();
+                    break;
+                case 2:
+                    System.out.println("проверить наличие категории в списке");
+                    Find.find();
+                    break;
 
-            case 3:
-                System.out.println("перемещение категорию");
-                Replace.move();
-                break;
+                case 3:
+                    System.out.println("перемещение категорию");
+                    Replace.move();
+                    break;
 
-            case 4:
-                System.out.println("удалить категорию");
-                DeleteCategoryQuery.deleteCategory();
+                case 4:
+                    System.out.println("удалить категорию");
+                    DeleteCategoryQuery.deleteCategory();
 
-                break;
+                    break;
 
-            default:
-                System.out.println("Вариант не существует,введите еще раз");
+                case 5:
+                    infinite = false;
+                    break;
+
+
+                default:
+                    System.out.println("Вариант не существует,введите еще раз");
+            }
         }
     }
 }
